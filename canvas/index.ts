e80921c789dd9c6bb1585b814ab7b127c3fb892c
Christopher Besch <christopher.besch@gmx.de>
2021-04-07 10:51:05 +0200

    animations working

--- canvas/index.ts
+++ canvas/index.ts
@@ -13,160 +13,223 @@ function get_ctx(id: string): CanvasRenderingContext2D {
 ///////////////////
 // simple shapes //
 ///////////////////
-let ctx_00: CanvasRenderingContext2D = get_ctx("canvas_00");
+{
+    let ctx: CanvasRenderingContext2D = get_ctx("canvas_00");
 
-// filled rectangle
-ctx_00.fillStyle = "green";
-ctx_00.fillRect(50, 50, 100, 100);
+    // filled rectangle
+    ctx.fillStyle = "green";
+    ctx.fillRect(50, 50, 100, 100);
 
-// outlined rectangle
-ctx_00.strokeStyle = "red";
-ctx_00.strokeRect(250, 50, 100, 100);
+    // outlined rectangle
+    ctx.strokeStyle = "red";
+    ctx.strokeRect(250, 50, 100, 100);
 
-// filled circle
-ctx_00.fillStyle = "blue";
-ctx_00.beginPath();
-ctx_00.arc(500, 100, 50, 0, Math.PI * 2);
-ctx_00.fill();
+    // filled circle
+    ctx.fillStyle = "blue";
+    ctx.beginPath();
+    ctx.arc(500, 100, 50, 0, Math.PI * 2);
+    ctx.fill();
 
-// outlined circle
-ctx_00.beginPath();
-ctx_00.arc(700, 100, 50, 0, Math.PI * 2);
-ctx_00.strokeStyle = "purple";
-ctx_00.stroke();
+    // outlined circle
+    ctx.beginPath();
+    ctx.arc(700, 100, 50, 0, Math.PI * 2);
+    ctx.strokeStyle = "purple";
+    ctx.stroke();
 
-// filled quarter circle
-ctx_00.beginPath();
-ctx_00.arc(100, 250, 50, 0, Math.PI / 2);
-// end point has to be center
-ctx_00.lineTo(100, 250);
-ctx_00.fillStyle = "pink";
-ctx_00.fill();
+    // filled quarter circle
+    ctx.beginPath();
+    ctx.arc(100, 250, 50, 0, Math.PI / 2);
+    // end point has to be center
+    ctx.lineTo(100, 250);
+    ctx.fillStyle = "pink";
+    ctx.fill();
+}
 
 //////////
 // path //
 //////////
-let ctx_01: CanvasRenderingContext2D = get_ctx("canvas_01");
+{
+    let ctx: CanvasRenderingContext2D = get_ctx("canvas_01");
 
-// draw outlined path
-ctx_01.beginPath();
-ctx_01.moveTo(100, 100);
-ctx_01.lineTo(200, 100);
-ctx_01.moveTo(100, 200);
-ctx_01.lineTo(50, 100);
-ctx_01.strokeStyle = "green";
-ctx_01.stroke();
+    // draw outlined path
+    ctx.beginPath();
+    ctx.moveTo(100, 100);
+    ctx.lineTo(200, 100);
+    ctx.moveTo(100, 200);
+    ctx.lineTo(50, 100);
+    ctx.strokeStyle = "green";
+    ctx.stroke();
 
-// draw filled path
-ctx_01.beginPath();
-ctx_01.moveTo(400, 100);
-ctx_01.lineTo(500, 100);
-// all points have to be connected
-ctx_01.lineTo(400, 200);
-ctx_01.lineTo(350, 100);
-// last and first point don't have to be connected
-ctx_01.fillStyle = "green";
-ctx_01.fill();
+    // draw filled path
+    ctx.beginPath();
+    ctx.moveTo(400, 100);
+    ctx.lineTo(500, 100);
+    // all points have to be connected
+    ctx.lineTo(400, 200);
+    ctx.lineTo(350, 100);
+    // last and first point don't have to be connected
+    ctx.fillStyle = "green";
+    ctx.fill();
 
-// circle
-ctx_01.beginPath();
-ctx_01.arc(650, 150, 50, 0, Math.PI / 2);
-ctx_01.fillStyle = "green";
-ctx_01.fill();
+    // circle
+    ctx.beginPath();
+    ctx.arc(650, 150, 50, 0, Math.PI / 2);
+    ctx.fillStyle = "green";
+    ctx.fill();
+}
 
 ////////////
 // images //
 ////////////
-let ctx_02: CanvasRenderingContext2D = get_ctx("canvas_02");
+{
+    let ctx: CanvasRenderingContext2D = get_ctx("canvas_02");
 
-function draw_rotated_image(
-    ctx: CanvasRenderingContext2D,
-    image: HTMLImageElement,
-    x: number,
-    y: number,
-    width: number,
-    height: number,
-    rotation: number
-) {
-    let half_width = width / 2;
-    let half_height = height / 2;
+    function draw_rotated_image(
+        ctx: CanvasRenderingContext2D,
+        image: HTMLImageElement,
+        x: number,
+        y: number,
+        width: number,
+        height: number,
+        rotation: number
+    ) {
+        let half_width = width / 2;
+        let half_height = height / 2;
 
-    ctx.save();
+        ctx.save();
 
-    ctx.translate(x + half_width, y + half_height);
-    ctx.rotate(rotation);
-    ctx.drawImage(image, -half_width, -half_height, width, height);
+        ctx.translate(x + half_width, y + half_height);
+        ctx.rotate(rotation);
+        ctx.drawImage(image, -half_width, -half_height, width, height);
 
-    ctx.restore();
-}
-
-function draw_image(ctx: CanvasRenderingContext2D, image: HTMLImageElement) {
-    // has image not yet loaded?
-    if (!image.complete) {
-        setTimeout(function () {
-            draw_image(ctx, image);
-        }, 50);
-        return;
+        ctx.restore();
     }
-    ctx.drawImage(image, 20, 20, 300, 300);
 
-    draw_rotated_image(ctx, image, 420, 20, 300, 300, Math.PI / 4);
-    draw_rotated_image(ctx, image, 20, 220, 300, 300, 0);
-    draw_rotated_image(ctx, image, 420, 220, 300, 300, -Math.PI / 4);
-    draw_rotated_image(ctx, image, 20, 420, 300, 300, Math.PI);
-    draw_rotated_image(ctx, image, 420, 420, 300, 300, Math.PI / 2);
+    function draw_image(
+        ctx: CanvasRenderingContext2D,
+        image: HTMLImageElement
+    ) {
+        // has image not yet loaded?
+        if (!image.complete) {
+            setTimeout(function () {
+                draw_image(ctx, image);
+            }, 50);
+            return;
+        }
+        ctx.drawImage(image, 20, 20, 300, 300);
+
+        draw_rotated_image(ctx, image, 420, 20, 300, 300, Math.PI / 4);
+        draw_rotated_image(ctx, image, 20, 220, 300, 300, 0);
+        draw_rotated_image(ctx, image, 420, 220, 300, 300, -Math.PI / 4);
+        draw_rotated_image(ctx, image, 20, 420, 300, 300, Math.PI);
+        draw_rotated_image(ctx, image, 420, 420, 300, 300, Math.PI / 2);
+    }
+
+    let image: HTMLImageElement = new Image();
+    image.src = "./images/blue_cat.jpg";
+    draw_image(ctx, image);
 }
-
-let image_00: HTMLImageElement = new Image();
-image_00.src = "./images/blue_cat.jpg";
-draw_image(ctx_02, image_00);
 
 ///////////////////////////////
 // image cropping and tiling //
 ///////////////////////////////
-let ctx_03: CanvasRenderingContext2D = get_ctx("canvas_03");
-let image_01 = new Image();
-image_01.src = "./images/cat.svg";
-image_01.onload = function () {
-    // (image, src_x, src_y, src_width, src_height, x, y, width, height)
-    ctx_03.drawImage(
-        image_01,
-        0,
-        0,
-        image_01.naturalWidth / 2,
-        image_01.naturalHeight / 2,
-        10,
-        10,
-        200,
-        200
-    );
-};
+{
+    let ctx: CanvasRenderingContext2D = get_ctx("canvas_03");
+    let image_00 = new Image();
+    image_00.src = "./images/cat.svg";
+    image_00.onload = function () {
+        // (image, src_x, src_y, src_width, src_height, x, y, width, height)
+        ctx.drawImage(
+            image_00,
+            0,
+            0,
+            image_00.naturalWidth / 2,
+            image_00.naturalHeight / 2,
+            10,
+            10,
+            200,
+            200
+        );
+    };
 
-let image_02 = new Image();
-image_02.onload = function () {
-    // create temporary canvas
-    let temp_canvas: HTMLCanvasElement = document.createElement("canvas");
-    temp_canvas.width = 50;
-    temp_canvas.height = 50;
-    let temp_ctx: CanvasRenderingContext2D = temp_canvas.getContext(
-        "2d"
-    ) as CanvasRenderingContext2D;
-    // draw image on temp_canvas
-    temp_ctx.drawImage(image_02, 0, 0, temp_canvas.width, temp_canvas.height);
+    let image_01 = new Image();
+    image_01.onload = function () {
+        // create temporary canvas
+        let temp_canvas: HTMLCanvasElement = document.createElement("canvas");
+        temp_canvas.width = 50;
+        temp_canvas.height = 50;
+        let temp_ctx: CanvasRenderingContext2D = temp_canvas.getContext(
+            "2d"
+        ) as CanvasRenderingContext2D;
+        // draw image on temp_canvas
+        temp_ctx.drawImage(
+            image_01,
+            0,
+            0,
+            temp_canvas.width,
+            temp_canvas.height
+        );
 
-    // repeat, repeat-x, repeat-y, no-repeat
-    let pattern: CanvasPattern = ctx_03.createPattern(
-        // can be image or canvas
-        temp_canvas,
-        "repeat"
-    ) as CanvasPattern;
-    // rectangle
-    ctx_03.fillStyle = pattern;
-    ctx_03.fillRect(10, 310, 300, 300);
+        // repeat, repeat-x, repeat-y, no-repeat
+        let pattern: CanvasPattern = ctx.createPattern(
+            // can be image or canvas
+            temp_canvas,
+            "repeat"
+        ) as CanvasPattern;
+        // rectangle
+        ctx.fillStyle = pattern;
+        ctx.fillRect(10, 310, 300, 300);
 
-    // circle
-    ctx_03.beginPath();
-    ctx_03.arc(500, 410, 100, 0, Math.PI * 2);
-    ctx_03.fill();
-};
-image_02.src = "./images/blue_cat.jpg";
+        // circle
+        ctx.beginPath();
+        ctx.arc(500, 410, 100, 0, Math.PI * 2);
+        ctx.fill();
+    };
+    image_01.src = "./images/blue_cat.jpg";
+}
+
+///////////////
+// animation //
+///////////////
+{
+    let ctx: CanvasRenderingContext2D = get_ctx("canvas_04");
+
+    let rectangle = { x: 0, y: 0, width: 100, height: 100 };
+    interface Rectangle {
+        x: number;
+        y: number;
+        width: number;
+        height: number;
+        color: string;
+        speed_x: number;
+    }
+
+    let rectangles: Rectangle[] = [
+        { x: 0, y: 0, width: 100, height: 100, color: "red", speed_x: 0.5 },
+        { x: 0, y: 100, width: 100, height: 100, color: "blue", speed_x: 1 },
+        { x: 0, y: 200, width: 100, height: 100, color: "green", speed_x: 2 },
+        { x: 0, y: 300, width: 100, height: 100, color: "orange", speed_x: 3 },
+        { x: 0, y: 400, width: 100, height: 100, color: "yellow", speed_x: 4 },
+        { x: 0, y: 500, width: 100, height: 100, color: "pink", speed_x: 5 },
+        { x: 0, y: 600, width: 100, height: 100, color: "purple", speed_x: 6 },
+    ];
+
+    function update() {
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+        rectangles.forEach(function (rectangle) {
+            ctx.fillStyle = rectangle.color;
+            ctx.fillRect(
+                rectangle.x,
+                rectangle.y,
+                rectangle.width,
+                rectangle.height
+            );
+            rectangle.x += rectangle.speed_x;
+        });
+
+        // call ASAP using double buffer
+        window.requestAnimationFrame(update);
+    }
+    update();
+}
