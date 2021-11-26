08edb41e94c404eefc6fdda969abbefea8902695
christopher-besch <christopher.besch@gmx.de>
2021-11-26 14:41:58 +0100

    added static stuff

--- gatsby/src/pages/projects/index.tsx
+++ gatsby/src/pages/projects/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Layout from "../../components/layout";
+import * as styles from "../../styles/projects.module.css";
 
 export default function Projects() {
     return (
         <Layout>
-            <div>Projects</div>
+            <div className={styles.portfolio}>
+                <h2>Portfolio</h2>
+                <h3>Projects & Webistes I've built</h3>
+            </div>
         </Layout>
     )
 }
